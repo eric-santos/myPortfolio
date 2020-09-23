0203d17a6e3cd02361168cff550aee6d15974c2b
eric <eric355402@yahoo.com>
2020-09-23 14:55:30 -0600

    changed background image of app.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 import "./styles/Home.css";
 
 const Home = () => {
-  return <div className="bkg-image">Home</div>;
+  return (
+    <div className="bkg-image">
+      <h1>Eric Santos</h1>
+      <h4>Full Stack Developer</h4>
+    </div>
+  );
 };
 
 export default Home;
