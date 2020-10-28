baa6fb942d042d505b9a9df3939246cc71a4b23d
eric santos <eric355402@yahoo.com>
2020-10-28 13:28:05 -0600

    updated about me section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,28 @@ import React from "react";
 import "./styles/About.css";
 
 const About = () => {
-  return <div className="height">Work In Progress</div>;
+  return (
+    <div className="height">
+      <p>
+        I am interested in Software Engineering and seeking exciting opportunities in Full-Stack or
+        Front-End Development. I am currently enrolled as a Full-Stack Developer in Lambda School
+        (Remote) scheduled to become endorsed by January 2021. Being a first generation American,
+        and Army Veteran. I have repeatedly been exposed to new cultures and diverse groups of
+        people.
+        <br />
+        <br />
+        This has made me quick to adapt to changes and gain a broader perspective. I am always
+        curious about things and enjoy learning. I like to solve problems, fix errors ,and I am
+        detail oriented. I keep my goals and tasks organized to maintain productivity.
+        <br />
+        <br />I have had the opportunity to become a Diesel Mechanic with United Rentals working in
+        Houston, TX and got promoted to the front office as an Inside Sales in the Trenching
+        department in El Paso, TX. I believe that the skills that I have attained from the dynamic
+        environment and competitive boot camp life define me and I look forward to implementing and
+        polishing these skills.
+      </p>
+    </div>
+  );
 };
 
 export default About;
